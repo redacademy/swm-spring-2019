5e0ce42cbc7bae853a329dd9f2c9e472c9c26451
nnalge123 <nathannagle1@gmail.com>
2019-06-19 17:47:42 -0400

    adding minor semantic name changes

--- themes/swm/page-thank-you.php
+++ themes/swm/page-thank-you.php
@@ -5,7 +5,7 @@
 <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 <section class="typ">
-<section class="backgroundImage"><img  src= "<?php echo get_template_directory_uri() . '/images/peace_cmyk.png'; ?>" alt= "Background Image of the starts with me logo" height="472" width="393"></section>
+<section class="backgroundImage"><img  src= "<?php echo get_template_directory_uri() . '/images/swmLogo.png'; ?>" alt= "Background Image of the starts with me logo" height="472" width="393"></section>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
